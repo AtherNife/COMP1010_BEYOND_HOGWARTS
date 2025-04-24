ce9e1a89fefad0b83c1b0490cfd90cb364f4128c
AtherNife <athernife123@gmail.com>
2025-04-24 21:59:55 +1000

    Create App.java

---
+++ App.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+public class App {
+public static String ChoosingTeam(int key){
+    if(key == 1){
+        return ("You Chose Team GRYFFINDOR!");
+    }else if(key == 2){
+        return("You Chose Team SLYTHERIN!");
+    }else{
+        return ("Invalid Choice! PREASE PRESS ~~~ 1 OR 2 ~~~");
+    }    
+}
+
+public static String ChoosingPlayer(int key){
+ static ArrayList<string> GRYFFINDOR = new ArrayList<>();
+ static ArrayList<string> SLYTHERIN = new ArrayList<>();
+}
+
+
+
+
+
+public static void main(String[] args){
+///Printing the name of the game
+ System.out.println("~~~~~~ WELCOME TO BEYOND HOGWARTS ~~~~~~~~\n");
+ /// object for choosing the team
+ Scanner scanner = new Scanner(System.in);
+ System.out.println("TO PLAY THE GAME PLEASE CHOOSE A TEAM FROM BELOW\n" + "1. TEAM GRYFFINDOR\n" + "2. TEAM SLYTHERIN" );
+ int input = scanner.nextInt();
+ String result = ChoosingTeam(input);
+ System.out.println(result);
+ scanner.close();
+}
+}
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
