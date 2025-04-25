cbcd9f3f6c724fedc2604d8670e63ca5cd747912
AtherNife <athernife123@gmail.com>
2025-04-25 11:40:13 +1000

    Adding Team Members
    Still in progress

--- HOGWARTS.java
+++ HOGWARTS.java
@@ -3,9 +3,9 @@ import java.util.ArrayList;
 public class HOGWARTS {
 public static String ChoosingTeam(int key){
     if(key == 1){
-        return ("You Chose Team GRYFFINDOR!");
+        return ("You Chose Team GRYFFINDOR!\n Now Please Select a Player:");
     }else if(key == 2){
-        return("You Chose Team SLYTHERIN!");
+        return("You Chose Team SLYTHERIN!\n Now Please Select a Player:1");
     }else{
         return ("Invalid Choice! PREASE PRESS ~~~ 1 OR 2 ~~~");
     } 
@@ -24,6 +24,14 @@ public static void main(String[] args){
  int input = scanner.nextInt();
  String result = ChoosingTeam(input);
  System.out.println(result);
+///ArrayList for team members and objects
+ArrayList<String> GRYFFINDOR = new ArrayList<>();
+ GRYFFINDOR.add("1. HARRY POTTER"); 
+ GRYFFINDOR.add("2. ALBUS DUMBLEDORE");
+ ArrayList<String> SLYTHERIN = new ArrayList<>();
+ SLYTHERIN.add("1. DRACO MALFOY"); 
+ SLYTHERIN.add("2. SEVERUS SNAPE");
+
  scanner.close();
 }
 }
