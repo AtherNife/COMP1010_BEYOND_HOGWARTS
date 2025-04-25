cbe84670ad22ceb4b0ec540c4f4f0e844440a623
AtherNife <athernife123@gmail.com>
2025-04-25 11:54:17 +1000

    Choosing team
    Changed the return type for choosing team to add more functionalities.

--- HOGWARTS.java
+++ HOGWARTS.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 public class HOGWARTS {
-public static String ChoosingTeam(int key){
+public static void ChoosingTeam(int key){
     if(key == 1){
-        return ("You Chose Team GRYFFINDOR!\n Now Please Select a Player:");
+        System.out.println("You Chose Team GRYFFINDOR!\n Now Please Select a Player:");
     }else if(key == 2){
-        return("You Chose Team SLYTHERIN!\n Now Please Select a Player:1");
+        System.out.println("You Chose Team SLYTHERIN!\n Now Please Select a Player:1");
     }else{
-        return ("Invalid Choice! PREASE PRESS ~~~ 1 OR 2 ~~~");
+        System.out.println("Invalid Choice! PREASE PRESS ~~~ 1 OR 2 ~~~");
     } 
        
 }
@@ -22,8 +22,7 @@ public static void main(String[] args){
  Scanner scanner = new Scanner(System.in);
  System.out.println("TO PLAY THE GAME PLEASE CHOOSE A TEAM FROM BELOW\n" + "1. TEAM GRYFFINDOR\n" + "2. TEAM SLYTHERIN" );
  int input = scanner.nextInt();
- String result = ChoosingTeam(input);
- System.out.println(result);
+ ChoosingTeam(input);
 ///ArrayList for team members and objects
 ArrayList<String> GRYFFINDOR = new ArrayList<>();
  GRYFFINDOR.add("1. HARRY POTTER"); 
